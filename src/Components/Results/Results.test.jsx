06ced6cbfac0c6e88b9094b4515ada4e0066ca1d
Adnan <adnanmohamud6@gmail.com>
2023-10-25 16:19:26 -0700

    Phase 3

--- src/Components/Results/Results.test.jsx
+++ src/Components/Results/Results.test.jsx
@@ -1,21 +1,35 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Results from '../Results';
 
 describe('<Results />', () => {
   it('renders "Loading..." when props.loading is true', () => {
-    const { getByText } = render(<Results loading={true} />);
-    expect(getByText('Loading...')).toBeInTheDocument();
+    render(<Results loading={true} />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 
-  it('renders the provided data as pretty JSON when props.data is provided', async () => {
+  it('renders JSON data when props.data is provided', () => {
     const testData = {
       key1: 'value1',
       key2: 'value2'
     };
 
-    const { findByText } = render(<Results data={testData} />);
+    render(<Results data={testData} />);
 
-    const formattedData = await findByText(/"key1": "value1",\s+"key2": "value2"/);
-    expect(formattedData).toBeInTheDocument();
+    // For ReactJson, it may render the keys and values as separate text nodes.
+    expect(screen.getByText(/key1/)).toBeInTheDocument();
+    expect(screen.getByText(/value1/)).toBeInTheDocument();
+    expect(screen.getByText(/key2/)).toBeInTheDocument();
+    expect(screen.getByText(/value2/)).toBeInTheDocument();
+  });
+
+  it('does not render JSON data when props.data is not provided', () => {
+    render(<Results data={null} />);
+    
+    // The assertion is a little trickier here because we want to make sure no JSON data is rendered.
+    // Using queryByText will not throw an error if the element isn't found, it just returns null.
+    expect(screen.queryByText(/key1/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/value1/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/key2/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/value2/)).not.toBeInTheDocument();
   });
 });
